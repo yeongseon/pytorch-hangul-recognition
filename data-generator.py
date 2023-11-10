4388f7332bea68157c72c7bb364bfac10c8d9eb2
Yeongseon Choe <yeongseon.choe@gmail.com>
2023-11-10 22:30:04 +0900

    Update data-generator.py

--- data-generator.py
+++ data-generator.py
@@ -8,16 +8,16 @@ font_dirs = [d for d in os.listdir("fonts") if os.path.isdir(os.path.join("fonts
 all_labels = []
 
 for font_dir in font_dirs:
-    output_dir = os.path.abspath(f"./output/output_{font_dir}")
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    image_data_dir = os.path.abspath(f"./image-data/{font_dir}")
+    if not os.path.exists(image_data_dir):
+        os.makedirs(image_data_dir)
 
     # 이미지 생성 명령 실행
-    command = f"python tools/hangul-image-generator.py --label-file labels/256-common-hangul.txt --font-dir ./fonts/{font_dir} --output-dir {output_dir}"
+    command = f"python tools/hangul-image-generator.py --label-file ./labels/256-common-hangul.txt --font-dir ./fonts/{font_dir} --output-dir {image_data_dir}"
     subprocess.run(command, shell=True, stderr=subprocess.DEVNULL)  # 에러 메시지 무시
 
     # labels-map.csv 파일 읽기
-    labels_map_file = os.path.join(output_dir, "labels-map.csv")
+    labels_map_file = os.path.join(image_data_dir, "labels-map.csv")
     if os.path.exists(labels_map_file):
         try:
             with open(labels_map_file, 'r', encoding='utf-8') as file:
@@ -26,5 +26,5 @@ for font_dir in font_dirs:
             print(f"Error reading {labels_map_file}: {e}")
 
 # 모든 labels-map.csv 내용을 하나의 파일로 병합
-with open("./output/labels-map.csv", 'w', encoding='utf-8') as merged_file:
+with open("./image-data/labels-map.csv", 'w', encoding='utf-8') as merged_file:
     merged_file.writelines(all_labels)
