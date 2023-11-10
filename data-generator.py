148019d12befbe04b72f388b59af52d389af16a8
Yeongseon Choe <yeongseon.choe@gmail.com>
2023-11-10 18:23:52 +0900

    Update fonts

---
+++ data-generator.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+# 'fonts' 디렉토리 내의 모든 하위 디렉토리 가져오기
+font_dirs = [d for d in os.listdir("fonts") if os.path.isdir(os.path.join("fonts", d))]
+
+# 명령 실행 및 labels-map.csv 파일 병합을 위한 준비
+all_labels = []
+
+for font_dir in font_dirs:
+    output_dir = os.path.abspath(f"./output/output_{font_dir}")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # 이미지 생성 명령 실행
+    command = f"python tools/hangul-image-generator.py --label-file labels/256-common-hangul.txt --font-dir ./fonts/{font_dir} --output-dir {output_dir}"
+    subprocess.run(command, shell=True, stderr=subprocess.DEVNULL)  # 에러 메시지 무시
+
+    # labels-map.csv 파일 읽기
+    labels_map_file = os.path.join(output_dir, "labels-map.csv")
+    if os.path.exists(labels_map_file):
+        try:
+            with open(labels_map_file, 'r', encoding='utf-8') as file:
+                all_labels.extend(file.readlines())
+        except UnicodeDecodeError as e:
+            print(f"Error reading {labels_map_file}: {e}")
+
+# 모든 labels-map.csv 내용을 하나의 파일로 병합
+with open("./output/labels-map.csv", 'w', encoding='utf-8') as merged_file:
+    merged_file.writelines(all_labels)
